7488f9aa67d2e52f37d9aa53c4c1286740638374
alfan06 <alfannoufal@gmail.com>
2025-06-07 11:20:16 +0700

    v0.2.0

--- src/LogRecord.php
+++ src/LogRecord.php
@@ -2,11 +2,16 @@
 
 namespace Alfan06\LaravelCustomLogging;
 
-class LogRecord extends \Monolog\LogRecordAdd commentMore actions
+class LogRecord extends \Monolog\LogRecord
 {
     public function toArray(): array
     {
         $array = parent::toArray();
+
+        // Remove datetime
+        unset($array['datetime']);
+
+        // Add origin
         return array_merge(['origin' => 'app.' . config('custom-logging.container_role')], $array);
     }
 }
